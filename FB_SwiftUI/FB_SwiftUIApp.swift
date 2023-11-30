a37beccd67ef282ab5ddd16ff32770a2e9be8bec
SreytouchLang <atouch.it@gmail.com>
2023-11-29 17:07:40 -0800

    Initial Commit

---
+++ FB_SwiftUI/FB_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FB_SwiftUIApp.swift
+//  FB_SwiftUI
+//
+//  Created by Sreytouch(Jessica) on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FB_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
