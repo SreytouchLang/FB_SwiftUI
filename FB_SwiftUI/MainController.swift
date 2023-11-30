439545cd7de7c28ec1d014d2d2157153a4fe9e13
SreytouchLang <atouch.it@gmail.com>
2023-11-30 13:20:35 -0800

    push code

---
+++ FB_SwiftUI/MainController.swift
@@ -0,0 +1,23 @@
+//
+//  MainController.swift
+//  FB_SwiftUI
+//
+//  Created by Sreytouch(Jessica) on 11/29/23.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class MainController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
+
+struct MainPreview: PreviewProvider {
+    static var previews: some View{
+        Text("main preview 123")
+    }
+}
